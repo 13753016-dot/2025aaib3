23108fbc5962f2ee368f7cf7d0d165edfcf45f6d
13753016-dot <13753016@me.mcu.edu.tw>
2025-09-10 11:54:59 +0800

    week01

---
+++ week 01-4a.cpp
@@ -0,0 +1,14 @@
+// week01-4a.cpp 使用 C 語言 處理
+#include <stdio.h>  // 使用 C 語言 函式庫
+
+int main()
+{
+    int a, b;
+    scanf("%d %d", &a, &b);   // C語言 讀資料
+    int ans = 0;
+    for (int i = a; i <= b; i++) {
+        if (i % 3 == 0) ans += i;
+    }
+    printf("%d", ans);  // C語言 輸資料
+    return 0;
+}
